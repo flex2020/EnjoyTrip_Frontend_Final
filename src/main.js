55fe574ea025988fa68be72978d7bceaf2852943
sbemjr1 <sbemjr1@naver.com>
2024-05-16 23:04:03 +0900

    design: 여행 후기 작성 페이지 작성

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import { useKakao } from 'vue3-kakao-maps/@utils';
 import App from './App.vue'
 import router from './router'
 
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
 const app = createApp(App)
+
+app.component('QuillEditor', QuillEditor)
 
 app.use(createPinia())
 app.use(router)
