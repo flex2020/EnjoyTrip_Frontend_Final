1909f65367253054e3d6a52b05e95eecc40e8a18
sbemjr1 <sbemjr1@naver.com>
2024-05-16 17:48:58 +0900

    design: 여행 후기 작성 페이지 설정 중

--- src/views/ReviewView.vue
+++ src/views/ReviewView.vue
@@ -3,8 +3,34 @@ import VHeader from "@/components/common/VHeader.vue";
 </script>
 
 <template>
-  <VHeader></VHeader>
-  <div>여행 후기 입니다.</div>
+  <VHeader background="white"></VHeader>
+  <div id="head-image">
+    <div>여행 후기 작성</div>
+    <div>좋았던 여행 후기를 공유해보세요</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#head-image {
+  width: 100%;
+  height: 500px;
+  background-image: url("/src/assets/img/reviewwrite.png");
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  color: white;
+  font-weight: bold;
+  font-size: 56px;
+  background-repeat: no-repeat; /* 이미지 반복 방지 */
+}
+#head-image div {
+  margin-bottom: 30px;
+}
+
+#head-image div:last-child {
+  font-size: 20px;
+  color: rgba(255, 255, 255, 0.8);
+  margin-bottom: 30px;
+}
+</style>
