f4743a1b57de3426ae9aa014a7dc418f33149440
dlduddnjs198 <dlduddnjs198@naver.com>
2024-05-20 13:54:06 +0900

    fix : pinia 새로고침시 데이터 유지

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
-import './assets/css/main.css'
+import "./assets/css/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { useKakao } from 'vue3-kakao-maps/@utils';
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { useKakao } from "vue3-kakao-maps/@utils";
+import App from "./App.vue";
+import router from "./router";
 
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import piniaPersist from "pinia-plugin-persist";
 
-const app = createApp(App)
+const app = createApp(App);
+// Pinia 생성 및 설정
+const pinia = createPinia();
+pinia.use(piniaPersist);
 
-app.component('QuillEditor', QuillEditor)
+app.component("QuillEditor", QuillEditor);
 
-app.use(createPinia())
-app.use(router)
+app.use(pinia); // pinia를 여기에 추가합니다.
+app.use(router);
 
 useKakao(import.meta.env.VITE_KAKAOMAP_SERVICE_KEY);
-app.mount('#app')
+app.mount("#app");
